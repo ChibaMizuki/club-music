023d35e28967c8618f037593cddb7f67ff47e7e5
Lai <lokutouzan@gmail.com>
2024-06-26 16:10:10 +0900

    編集機能、css

--- music/urls.py
+++ music/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import SongListView, SongCreateView, SongDeleteView, SongDetailView
+from .views import SongListView, SongCreateView, SongDeleteView, SongDetailView, SongEditView
 
 urlpatterns = [
     path('', SongListView.as_view(), name='home'),
     path('registration/', SongCreateView.as_view(), name='registration'),
-    path('delete/<int:pk>/', SongDeleteView.as_view(), name='delete'),
-    path('detail/<int:pk>/', SongDetailView.as_view(), name='detail'),
+    path('<int:pk>/', SongDetailView.as_view(), name='detail'),
+     path('song/<int:pk>/edit/', SongEditView.as_view(), name='edit'),
+     path('<int:pk>/delete/', SongDeleteView.as_view(), name='delete'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView, DeleteView, DetailView
+from django.views.generic import ListView, CreateView, DeleteView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from .models import Song
 from .forms import SongForm
@@ -16,18 +16,28 @@ class SongListView(ListView):
             song.display_number = index
         return context
 
+
 class SongCreateView(CreateView):
     model = Song
     form_class = SongForm
     template_name = 'song_form.html'
     success_url = reverse_lazy('home')
-    
+
+
 class SongDeleteView(DeleteView):
     model = Song
     template_name = 'song_delete.html'
     success_url = reverse_lazy('home')
-    
+
+
 class SongDetailView(DetailView):
     model = Song
     template_name = 'song_detail.html'
     context_object_name = 'song'
+
+
+class SongEditView(UpdateView):
+    model = Song
+    form_class = SongForm
+    template_name = 'song_form.html'
+    success_url = reverse_lazy('home')
